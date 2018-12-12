64485170ac6a9acc902873df1a5f4eac9abf312a
goh.jiaying <goh.jiaying1@dhs.sg>
2018-12-12 17:07:38 +0800

    Commit work before implementing deployment pipeline

---
+++ README.md
@@ -0,0 +1,5 @@
+# Help Me Buy Serverless
+
+# Resources Used
+https://aws.amazon.com/getting-started/projects/build-serverless-web-app-lambda-apigateway-s3-dynamodb-cognito/
+https://github.com/serverless/examples/blob/master/aws-python-rest-api-with-dynamodb/todos/create.py
--- hmbCreateItem.py
+++ hmbCreateItem.py
@@ -1,22 +1,29 @@
 import json
 import boto3
 import random
+import logging
+import os
 
 #define dynamodb item variables
-hmb_item_table = "Items"
+hmb_item_table = os.environ['ITEMS_TABLE']
+hmb_item_table_required_field = os.environ['ITEMS_TABLE_REQUIRED_FIELDS']
 
 #define boto variables
 dynamodb = boto3.client('dynamodb')
 
 
-def boto_put_item (table_name, item) :
-    response = dynamodb.put_item(
-        TableName = table_name,
-        Item = item
-    )
-    return response
-
-
+def generate_item_id ():
+    return random.randint(1,999999)
+    
+    
+def is_valid_shopping_data (data) :
+    for field in hmb_item_table_required_field:
+        if field not in data:
+            logging.error("Validation Failed")
+            raise Exception("Couldn't create the shopping item.")
+            return
+        
+        
 def put_shopping_item (item_id, item_name, item_price):
     item = {}
     item['ItemId'] = {"N": item_id}
@@ -27,12 +34,23 @@ def put_shopping_item (item_id, item_name, item_price):
     return response
 
 
-def get_item_id ():
-    return random.randint(1,999999)
-
-
-def lambda_handler (event, context):
-    # TODO implement
-    response = put_shopping_item(get_item_id(), event['name'], event['price'])
-    
+def boto_put_item (table_name, item) :
+    response = dynamodb.put_item(
+        TableName = table_name,
+        Item = item
+    )
     return response
+
+
+def create_item (event, context):
+    # TODO implement
+    data = json.loads(event['body'])
+    response = put_shopping_item(str(generate_item_id()), data['name'], data['price'])
+    print(response)
+    
+    to_return = {
+        "statusCode": 200,
+        "body": json.dumps(data)
+    }
+
+    return to_return
